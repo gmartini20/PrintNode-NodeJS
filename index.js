d4daf62dc69597f60dae0fa51353a2bbaf7adaca
Guilherme Martini <guilherme.martini20@gmail.com>
2018-08-01 14:05:38 -0300

    added method to create childAccount

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var _ = require('lodash');
 var client = require('./lib/client');
 
 _.assign(client.prototype, require('./lib/account-information'));
-// _.assign(client.prototype, require('./lib/account-management'));
+_.assign(client.prototype, require('./lib/account-management'));
 _.assign(client.prototype, require('./lib/computers'));
 _.assign(client.prototype, require('./lib/printers'));
 _.assign(client.prototype, require('./lib/printjobs'));
